c27e60cd6e478755768d2756349e35eb04ec738e
Rino Cala <rino.cala1998@gmail.com>
2019-07-04 17:39:37 +0200

    XCode initial setup

---
+++ TVShows/TVShows/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TVShows
+//
+//  Created by Rino Čala on 04/07/2019.
+//  Copyright © 2019 Infinum Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
