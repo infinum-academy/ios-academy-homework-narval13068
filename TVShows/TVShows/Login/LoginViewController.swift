752239b74845952b21e84e6415ff3a59887f9654
Rino Cala <rino.cala1998@gmail.com>
2019-07-12 12:40:10 +0200

    This work contains added Login Screen with features : 	- username field 	- password field 	- remember me checkbox 	- log in button 	- create account button

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -2,76 +2,55 @@
 //  LoginViewController.swift
 //  TVShows
 //
-//  Created by Rino Čala on 05/07/2019.
+//  Created by Rino Čala on 11/07/2019.
 //  Copyright © 2019 Infinum Academy. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-final class LoginViewController : UIViewController {
+final class LoginViewController : UIViewController,UITextFieldDelegate {
     
-    // MARK: - Outlets
+    // MARK - Outlets
     
-    @IBOutlet private weak var sumLabel: UILabel!
-    @IBOutlet private weak var increaseButton: UIButton!
-    @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
-    @IBOutlet private weak var changeStateButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var rememberMeButton: UIButton!
+    @IBOutlet private weak var usernameField: UITextField!
+    @IBOutlet private weak var passwordField: UITextField!
     
-    // MARK: - Properties
-    
-    private var numberOfClicks: Int = 0
-    
-    // MARK: - Lifecycle methods
+    // MARK - Lifecycle methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initialConfigureUI()
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        onAppearConfigureUI()
-    }
-    
-    // MARK: - Initial configuration of UI
+    // MARK - UI Configure
     
     private func initialConfigureUI() {
-        sumLabel.text = String(numberOfClicks)
-        increaseButton.layer.cornerRadius = 10
-        increaseButton.clipsToBounds = true
-        changeStateButton.layer.cornerRadius = 10
-        changeStateButton.clipsToBounds = true
+        loginButton.layer.cornerRadius = 5
+        scrollView.showsVerticalScrollIndicator = false
+        usernameField.delegate = self
+        passwordField.delegate = self
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(viewTapped)))
     }
     
-    // MARK: - On appear configuration of UI
+    // MARK - Actions
     
-    private func onAppearConfigureUI() {
-        activityIndicator.startAnimating()
-        changeStateButton.isHidden = true
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.activityIndicator.stopAnimating()
-            self.changeStateButton.isHidden = false
-        }
+    @IBAction private func rememberMeButtonTapped(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
     }
     
-    // MARK: - Actions
+    // MARK - Dismissing keyboard
     
-    @IBAction private func increaseButtonTapped() {
-        activityIndicator.startAnimating()
-        numberOfClicks += 1
-        sumLabel.text = String(numberOfClicks)
-        activityIndicator.stopAnimating()
+    @objc private func viewTapped (sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
     }
     
-    @IBAction private func changeStateButtonTapped() {
-        if activityIndicator.isAnimating {
-            activityIndicator.stopAnimating()
-            changeStateButton.setTitle("Start", for: .normal)
-        } else {
-            activityIndicator.startAnimating()
-            changeStateButton.setTitle("Stop", for: .normal)
-        }
+    internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
-
+    
 }
